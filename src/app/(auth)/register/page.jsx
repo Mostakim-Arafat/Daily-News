d3dc6c43f97a882db9f7d6cedaa2e049e41f12cf
Mostakim <mostakim2005arafat@gmail.com>
2026-05-08 16:12:51 +0600

    add better auth

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,9 +1,40 @@
+'use client'
+import { useForm } from "react-hook-form"
 
 
 const RegisterPage = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
+
+    const handleRegister = (data) => {
+       console.log(data)
+    }
     return (
-        <div>
-            registerPage
+        <div className="container mx-auto  mt-10 bg-slate-200 flex flex-col justify-center items-center">
+            <form onSubmit={handleSubmit(handleRegister)}>
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4 my-10">
+                    <label className="label">Name</label>
+                    <input type="text" className="input" placeholder=" Your Name" {...register("name", { required:'name required'})}/>
+                     {errors.name && <p className="text-red-400">{errors.name.message}</p>}
+
+                    <label className="label">Phone number</label>
+                    <input type="number" className="input" placeholder="Phone number" {...register("ph_number", { required: 'phone number required' })}/>
+                     {errors.ph_number && <p className="text-red-400">{errors.ph_number.message}</p>}
+
+                    <label className="label">Email</label>
+                    <input type="email" className="input" placeholder="Email" {...register("email", { required: 'email required' })} />
+                    {errors.email && <p className="text-red-400">{errors.email.message}</p>}
+
+                    <label className="label">Password</label>
+                    <input type="password" className="input" placeholder="Password" {...register("password", { required: 'password required' })} />
+                         {errors.password && <p className="text-red-400">{errors.password.message}</p>}
+
+                    <button className="btn btn-neutral mt-4">Register</button>
+                </fieldset>
+            </form>
         </div>
     );
 };
